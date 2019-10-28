a276fd5dc9ae59957474936ea150ea0dac9ea891
Xtek <Xtek@Pc-Nasa>
2019-10-28 17:36:40 +0100

    :metal: Commit resubido

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package alturapersonaconsola;
+package org.japo.java.main;
+
 
 import java.util.Scanner;
 
